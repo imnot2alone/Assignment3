1cf6b9c42c95ccb3fb82521009e0ee8b9c144b99
Tezzyboy <fraser.fwn@gmail.com>
2025-10-31 17:50:52 +1100

    Finished bird ai movement with waypoint seeking

---
+++ new_group_repository-main/new_group_repository-main/Assignment_3/Assets/Scripts/Projectile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+
+public class Projectile : MonoBehaviour
+{
+    public float lifeTime = 5f;
+
+
+    private void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(gameObject);
+    }
+}
